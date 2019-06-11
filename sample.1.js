3a0d21dec16c1a8955c56aa135da320f5559dc19
Ramesh <ramesh@rsageventures.com>
2019-06-11 21:20:30 +0530

    save file data to db

--- sample.1.js
+++ sample.1.js
@@ -1,9 +1,34 @@
 var idbAdapter = new LokiIndexedAdapter('DATABASETEST');
-idbAdapter.getDatabaseList(function(result) {
-    // result is array of string names for that appcontext ('finance')
-    result.forEach(function(str) {
-      console.log(str);
-    });
-  });
+// idbAdapter.getDatabaseList(function(result) {
+//     // result is array of string names for that appcontext ('finance')
+//     result.forEach(function(str) {
+//       console.log(str);
+//     });
+//   });
+// let database = idbAdapter.loadDatabase('usersDb.json');
+var db = new loki('usersDb.json', {
+  autosave: true,
+  autoload: true,
+  adapter: idbAdapter,
+  autosaveInterval: 1000
+});
 
-  
+console.log(db);
+//idbAdapter.loadDatabase('usersDb.json');
+db.loadDatabase({}, function (result) {
+  db.addCollection('users1');
+  console.log('result: ' + result);
+ console.log(db.getCollection("users1").find());
+console.log(db);
+ idbAdapter.saveDatabase('usersDb.json', JSON.stringify(db), function(result) {});
+ console.log("DNOE")
+});
+//console.log(db.getCollection("users").find());
+// var users = db.getCollection("users");
+// console.log("users: " + users);
+// if(!users) {
+//   alert("users null");
+//     users = db.addCollection("users");
+// }
+
+console.log(db.getCollection("users1"));
